587d32e9f9044e6ebf2fde24e15748bf06bf21c6
prince patel <164193414+PRINCE-BODA@users.noreply.github.com>
2024-07-25 20:57:48 +0530

    Add files via upload

---
+++ .05/address1.c
@@ -0,0 +1,7 @@
+// Print your address i) using single printf
+
+#include <stdio.h>
+void main()
+{
+	printf("Darshan University, Rajkot-Morbi Highway Hadala 123456");
+}
---
+++ .05/address2.c
@@ -0,0 +1,7 @@
+// Print your address i) using (\n)
+
+#include <stdio.h>
+void main()
+{
+	printf("Darshan University \nRajkot-Morbi Highway \nHadala \n123456");
+}
